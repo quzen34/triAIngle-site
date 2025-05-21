2bc6a8adc483ec5dac4ccc391e06d83f3b2ec148
quzen34 <fatihernalban@gmail.com>
2025-05-21 11:56:53 +0300

    fix: correct base for triAIngle custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/triAIngle-site/',
+  base: '/',  // SADECE SLASH! BAŞKA ŞEY YOK!
   plugins: [react()],
 })
